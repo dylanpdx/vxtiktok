41b06d76096d5337b1867f1014a020e7fa6aa235
Dylan <dylanpdx@gmail.com>
2022-10-11 00:42:16 +0100

    Serverless setup

--- cache.py
+++ cache.py
@@ -1,14 +1,19 @@
 from codecs import getdecoder
 import config
 import pymongo
+import boto3
 from datetime import date,datetime, timedelta
 
 client=None
 db=None
+DYNAMO_CACHE_TBL=None
 if config.currentConfig["CACHE"]["cacheMethod"] == "mongodb":
     client = pymongo.MongoClient(config.currentConfig["CACHE"]["databaseurl"], connect=False)
     table = config.currentConfig["CACHE"]["databasetable"]
     db = client[table]
+elif config.currentConfig["CACHE"]["cacheMethod"]=="dynamodb":
+    DYNAMO_CACHE_TBL=config.currentConfig["CACHE"]["databasetable"]
+    client = boto3.resource('dynamodb')
 
 def getDefaultTTL():
     return datetime.today().replace(microsecond=0) + timedelta(days=1)
@@ -20,6 +25,11 @@ def addToCache(url,vidInfo):
         elif config.currentConfig["CACHE"]["cacheMethod"] == "mongodb":
             ttl=getDefaultTTL()
             db.linkCache.insert_one({"url":url,"info":vidInfo,"ttl":ttl})
+        elif config.currentConfig["CACHE"]["cacheMethod"] == "dynamodb":
+            ttl=getDefaultTTL()
+            ttl = int(ttl.strftime('%s'))
+            table = client.Table(DYNAMO_CACHE_TBL)
+            table.put_item(Item={"url":url,"info":vidInfo,"ttl":ttl})
     except Exception as e:
         print("addToCache for URL "+url+" failed: "+str(e))
 
@@ -32,6 +42,11 @@ def getFromCache(url):
             if obj == None:
                 return None
             return obj["info"]
+        elif config.currentConfig["CACHE"]["cacheMethod"] == "dynamodb":
+            obj = client.Table(DYNAMO_CACHE_TBL).get_item(Key={'url': url})
+            if 'Item' not in obj:
+                return None
+            return obj["Item"]["info"]
     except Exception as e:
         print("getFromCache for URL "+url+" failed: "+str(e))
         return None
--- config.py
+++ config.py
@@ -19,10 +19,24 @@ currentConfig["CACHE"]={
     "cacheTTL":86400,
 }
 
-if os.path.exists("vxTiktok.conf"):
-    # as per python docs, "the most recently added configuration has the highest priority"
-    # "conflicting keys are taken from the more recent configuration while the previously existing keys are retained"
-    currentConfig.read("vxTiktok.conf")
+if 'RUNNING_SERVERLESS' in os.environ and os.environ['RUNNING_SERVERLESS'] == '1':
+    currentConfig["MAIN"]={
+        "appName": os.environ['APP_NAME'],
+        "embedColor": "#EE1D52",
+        "repoURL":os.environ['REPO_URL'],
+        "domainName":os.environ['DOMAINNAME'],
+    }
+    currentConfig["CACHE"]={
+        "cacheMethod":os.environ['CACHE_METHOD'],
+        "databaseURL":os.environ['DATABASE_URL'],
+        "databaseTable":os.environ['CACHE_TABLE'],
+        "cacheTTL":int(os.environ['CACHE_TTL']),
+    }
+else:
+    if os.path.exists("vxTiktok.conf"):
+        # as per python docs, "the most recently added configuration has the highest priority"
+        # "conflicting keys are taken from the more recent configuration while the previously existing keys are retained"
+        currentConfig.read("vxTiktok.conf")
 
-with open("vxTiktok.conf", "w") as configfile:
-    currentConfig.write(configfile) # write current config to file
+    with open("vxTiktok.conf", "w") as configfile:
+        currentConfig.write(configfile) # write current config to file
